c4fd0114173c02cd30d70c7af34416045ec1917d
Przemysław Kobylański <przemko@mac.com>
2017-01-27 16:08:17 +0100

    dodano tworzenie i otwarcie okienka

--- src/okienko.adb
+++ src/okienko.adb
@@ -1,6 +1,16 @@
+with Gtk.Main;
+with Gtk.Enums;
 with Gtk.Window;
 
 procedure Okienko is
+   Main_Window : Gtk.Window.Gtk_Window;
 begin
-   null;
+   --Gtk.Main.Set_Locale;
+   Gtk.Main.Init;
+   Gtk.Window.Gtk_New (Window => Main_Window,
+                       The_Type => Gtk.Enums.Window_Toplevel);
+   Gtk.Window.Set_Title (Window => Main_Window,
+                         Title => "Glowne okienko");
+   Gtk.Window.Show_All (Main_Window);
+   Gtk.Main.Main;
 end Okienko;
